fbc08a67deee84729e763277c891680764242d89
VibinLub <lubbertschenk@hotmail.com>
2024-07-29 21:56:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Sample project data
     const projects = [
       { id: 1, title: 'NinjagoMedia', owned: 'Yes', description: 'The place for all Ninjago content!', status: '63% Done', image: 'images/project1.jpg', link: 'https://vibinlub.github.io/NinjagoMedia/' },
-      { id: 2, title: '#Soon', owned: 'No Information At The Moment', description: '#Soon', status: '0% Done', image: './images/soon.jpg', link: '#Soon' },
+      { id: 2, title: '#Soon', owned: '#Soon', description: '#Soon', status: '0% Done', image: './images/soon.jpg', link: '#Soon' },
       // Add more projects as needed
     ];
   
