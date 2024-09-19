49b9028cc0865199d8ca5df9d1c4068472aa35a4
VibinLub <lubbertschenk@hotmail.com>
2024-09-19 11:41:10 +0200

    added 1 page and 1 project

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const projects = [
       { id: 1, title: 'NinjagoMedia', owned: 'Yes', description: 'The place for all Ninjago content!', status: '73% Done', image: 'images/project1.jpg', link: 'https://vibinlub.github.io/NinjagoMedia/' },
       { id: 2, title: 'FireSearch', owned: 'Yes', description: 'A Google Based Search Engine', status: '100% Done', image: './images/project2.png', link: 'https://vibinlub.github.io/FireSearch/' },
+      { id: 3, title: 'Dart Scores', owned: 'Yes', description: 'The best way to count your darting game!', status: 'Not Started Yet', image: './images/project3.png', link: 'https://vibinlub.github.io/NotStartedYet.html'}
       // Add more projects as needed
     ];
   
